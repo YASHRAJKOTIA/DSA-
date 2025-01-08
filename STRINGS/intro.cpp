c39c039e49ab78d9caa98c8d3a7246ae7db61973
YASH RAJKOTIA <146333489+YASHRAJKOTIA@users.noreply.github.com>
2025-01-08 22:29:46 +0530

    Add files via upload

---
+++ STRINGS/intro.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	string name = "YASH RAJKOTIA";
+	cout<<"MY NAME IS "<<name<<endl;
+	
+	name="BHAVYA RAJKOTIA";
+	cout<<"BROTHERS NAME IS "<<name<<endl;
+	
+	}
